8521781840d66587afdd014a801eb356673b1c48
962027 <962027@PITLAB2178776.senac.educ>
2024-02-20 07:47:54 -0300

    calculadora com métodos e pacotes

---
+++ conta/src/calculo/calc.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package calculo;
+
+/**
+ *
+ * @author 962027
+ */
+public class calc {
+     public static int somar(int a, int b){
+         int x;
+         x = a +b;
+         return x;
+}
+     
+     public static int sub(int a, int b){
+         int x;
+         x = a - b;
+         return x;
+}
+     public static int div(int a, int b){
+         int x;
+         x = a / b;
+         return x;
+}
+      public static int multi(int a, int b){
+         int x;
+         x = a * b;
+         return x;
+} 
+}
